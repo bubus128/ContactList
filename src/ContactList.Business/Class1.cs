9465541ad907591789bbddc88107a23e39c7ecd7
bubus128 <j.swislocki@outlook.com>
2025-09-14 10:47:31 +0200

    Init solution

---
+++ src/ContactList.Business/Class1.cs
@@ -0,0 +1,5 @@
+﻿namespace ContactList.Business;
+
+public class Class1
+{
+}
---
+++ src/ContactList.Data/Entities/Contact.cs
@@ -0,0 +1,11 @@
+namespace ContactList.Data.Entities;
+
+public class Contact
+{
+    public Guid Id { get; set; }
+    public string FirstName { get; set; } = null!;
+    public string LastName { get; set; } = null!;
+    public string? Email { get; set; }
+    public string? Phone { get; set; }
+    public Category Category { get; set; } = null!;
+}
