7bc23efba647c7a73b8e9d1813739e79391d1d4c
Jakub Świsłocki <72924140+bubus128@users.noreply.github.com>
2025-09-16 09:29:33 +0200

    (#6) Feature/6/tests init (#8)

---
+++ src/test/ContactList.UnitTest/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace ContactList.UnitTest;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
